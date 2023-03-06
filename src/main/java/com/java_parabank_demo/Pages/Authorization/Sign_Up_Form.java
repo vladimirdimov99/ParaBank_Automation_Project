742c0c070296c7ca9b527613fd38b857d7f0238a
Vladimir <vladimirdimov99@gmail.com>
2023-03-06 20:10:28 +0200

    Upload new changes

--- src/main/java/com/java_parabank_demo/Pages/Authorization/Sign_Up_Form.java
+++ src/main/java/com/java_parabank_demo/Pages/Authorization/Sign_Up_Form.java
@@ -4,12 +4,4 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Sign_Up_Form {
-    WebDriver driver;
-
-    public Sign_Up_Form(WebDriver driver) {this.driver = driver;}
-
-    By registerButton = By.linkText("Register");
-    public void GoToTheSignUpForm(){
-        driver.findElement(registerButton).click();
-    }
 }
